5a492a8af85049072e4f0bf72a6b28b069776f88
Quique Supertramp <quiquesupertramp@gmail.com>
2026-01-12 20:24:53 +0100

    cron healthz

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
+import { CronJob } from "cron"
 import app from "./app.js"
 import { ENV } from "./env.js"
 
 app.listen(ENV.PORT, () => {
   console.log(`API is running at http://localhost:${ENV.PORT}`)
 })
+
+new CronJob("*/14 * * * *", async () => {
+  const data = await fetch("https://feb-scouting-backend.onrender.com/healthz")
+  console.log("health:", `(${data.status}) => ${data.statusText}`)
+}).start()
