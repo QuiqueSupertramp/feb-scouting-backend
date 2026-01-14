e739927c8cdfd22df3d41ce5433ac58755257174
Quique Supertramp <quiquesupertramp@gmail.com>
2026-01-14 16:39:54 +0100

    fix classification

--- src/context/api/modules/scores/helpers/classification.ts
+++ src/context/api/modules/scores/helpers/classification.ts
@@ -96,18 +96,21 @@ export const getClassifications = (scores: Score[]): Classifications => {
       .map((t) => ({ teamFebId: t.teamFebId, ...t.total }))
       .sort((a, b) => {
         if (b.wins !== a.wins) return b.wins - a.wins
+        if (b.losses !== a.losses) return a.losses - b.losses
         return b.points - b.pointsAgainst - (a.points - a.pointsAgainst)
       }),
     local: teamsTable
       .map((t) => ({ teamFebId: t.teamFebId, ...t.local }))
       .sort((a, b) => {
         if (b.wins !== a.wins) return b.wins - a.wins
+        if (b.losses !== a.losses) return a.losses - b.losses
         return b.points - b.pointsAgainst - (a.points - a.pointsAgainst)
       }),
     away: teamsTable
       .map((t) => ({ teamFebId: t.teamFebId, ...t.away }))
       .sort((a, b) => {
         if (b.wins !== a.wins) return b.wins - a.wins
+        if (b.losses !== a.losses) return a.losses - b.losses
         return b.points - b.pointsAgainst - (a.points - a.pointsAgainst)
       }),
   }
